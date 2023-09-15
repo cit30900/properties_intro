0286eb0f51b95bbabce2681fe02af0025527fa64
Rob Elliott <elliott@iupui.edu>
2023-09-15 15:40:18 -0400

    Logic code in index.py

--- index.py
+++ index.py
@@ -1,7 +1,17 @@
 from shapes import Rectangle, Triangle
 
 def main():
-  pass
+  r1 = Rectangle(2,2)
+  r2 = Rectangle(3,5)
+
+  print(f'The area of r1 is {r1.area}.')
+  print(f'The perimeter of r2 is {r2.perimeter}.')
+
+  t1 = Triangle(3, 3, 3, 5)
+  t2 = Triangle(3, 5, 7, 6)
+
+  print(f'The area of t1 is {t1.area}.')
+  print(f'The perimeter of t2 is {t2.perimeter}.')
 
 if __name__== "__main__":
   main()
