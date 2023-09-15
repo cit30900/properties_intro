51b18e5092e30fbbb07c283aa366743500728a78
Rob Elliott <elliott@iupui.edu>
2023-09-15 15:24:04 -0400

    More typos

--- shapes_midway.py
+++ shapes_midway.py
@@ -34,8 +34,8 @@ class Triangle(Shape):
         super().__init__(side1, side2)
 
     def area(self, side3: float, height: float) -> float:
-        """Calculates the area of a triangle with the formula side3*height"""
-        return side3 * height
+        """Calculates the area of a triangle with the formula side3*height*.5"""
+        return side3 * height * .5
     
     def perimeter(self, side1: float, side2: float, side3: float) -> float:
         """Calculates the perimeter of a triangle with the formula side1+side2+side3"""
